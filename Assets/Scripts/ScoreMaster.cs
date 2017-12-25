8cc6b1124903f34f1b93d407f4a37b85b72a0b80
Marc Taurasi <altairre.atsu@gmail.com>
2017-12-25 14:13:41 -0700

    Reset Commit

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ScoreMaster {
 
+	public enum scoreAction {STRIKE_END_TURN, SPARE_END_TURN, DO_NOTHING};
+
 	// Returns a list of cumulative scores like a score card
 	public static List<int> ScoreCumulative (List<int> rolls) {
 		List<int> cumulativeScores = new List<int> ();
@@ -20,44 +22,10 @@ public class ScoreMaster {
 	// Returns a list of individual scores, not cumulative
 	public static List<int> ScoreFrames (List<int> rolls){
 		List<int> frameList = new List<int> ();
-		//int runningTotal = 0;
-		int index = 1;
-		int lastFrameEnd = 0;
-		int holdIndex;
-		bool gotStrike = false;
-		bool gotSpare = false;
 
 		// Loop over every value in rolls and calculate the score for that frame
 		foreach (int roll in rolls) {
-			bool endOfFrame;
 
-			if(roll == 10){
-				endOfFrame = true;
-				holdIndex = index;
-				gotStrike = true;
-				lastFrameEnd = index;
-
-			} else if ( (index > 1) && (roll + rolls[index - 2] == 10) && (index - lastFrameEnd == 2) ) {
-				endOfFrame = true;
-				holdIndex = index;
-				gotSpare = true;
-				lastFrameEnd = index;
-
-			} else if ( index - lastFrameEnd == 2) {
-				endOfFrame = true;
-				lastFrameEnd = index;
-			} else {
-				endOfFrame = false;
-			}
-
-			if (endOfFrame && !gotStrike && !gotSpare) {
-				int frameScore = roll + rolls [index - 2];
-				frameList.Add (frameScore);
-				Debug.Log ("End Of Frame, Added: " + frameScore);
-			}
-
-			index++;
-		}
 
 
 		return frameList;
